c0fcc1fe16d53ec19a36de901463009feec76de3
ZetHange <maksselles@gmail.com>
2023-04-08 20:08:23 +0300

    refactor: minimize project screen fix: changing the color when hovering over the button

--- src/App.tsx
+++ src/App.tsx
@@ -14,17 +14,59 @@ const App = () => {
 
   return (
     <>
-      <Box backgroundColor="#111827" w="100vw" h="100vh" display="flex" justifyContent="center" fontFamily="Comfortaa" textColor="white">
-        <Box backgroundColor="#1f2937" px="4" maxW='container.xl' h="100vh" overflowY="auto" mx="auto">
+      <Box
+        backgroundColor="#111827"
+        w="100vw"
+        h="100vh"
+        display="flex"
+        justifyContent="center"
+        fontFamily="Comfortaa"
+        textColor="white"
+      >
+        <Box
+          backgroundColor="#1f2937"
+          px="4"
+          maxW="container.xl"
+          h="100vh"
+          overflowY="auto"
+          mx="auto"
+        >
           <Navbar>
             <Link to="/">
-              <Button w="100%" background="linear-gradient(90deg, rgba(255,0,170,1) 0%, rgba(255,0,0,1) 100%);">Главная</Button>
+              <Button
+                w="100%"
+                background="linear-gradient(90deg, rgba(255,0,170,1) 0%, rgba(255,0,0,1) 100%);"
+                _hover={{
+                  background:
+                    "linear-gradient(90deg, rgba(255,0,0,1) 0%, rgba(255,0,170,1) 100%);",
+                }}
+              >
+                Главная
+              </Button>
             </Link>
             <Link to="/work">
-              <Button w="100%" background="linear-gradient(90deg, rgba(255,0,170,1) 0%, rgba(255,0,0,1) 100%);">Работа у нас</Button>
+              <Button
+                w="100%"
+                background="linear-gradient(90deg, rgba(255,0,170,1) 0%, rgba(255,0,0,1) 100%);"
+                _hover={{
+                  background:
+                    "linear-gradient(90deg, rgba(255,0,0,1) 0%, rgba(255,0,170,1) 100%);",
+                }}
+              >
+                Работа у нас
+              </Button>
             </Link>
             <Link to="/projects">
-              <Button w="100%" background="linear-gradient(90deg, rgba(255,0,170,1) 0%, rgba(255,0,0,1) 100%);">Наши проекты</Button>
+              <Button
+                w="100%"
+                background="linear-gradient(90deg, rgba(255,0,170,1) 0%, rgba(255,0,0,1) 100%);"
+                _hover={{
+                  background:
+                    "linear-gradient(90deg, rgba(255,0,0,1) 0%, rgba(255,0,170,1) 100%);",
+                }}
+              >
+                Наши проекты
+              </Button>
             </Link>
           </Navbar>
           <Routes>
